341196f55b47d5536f85537dd2fa49281b3b6bc6
Anshika-git13 <anshikabhatia1013@gmail.com>
2025-07-07 12:18:09 -0700

    ML

--- client/src/components/EventRecommender.js
+++ client/src/components/EventRecommender.js
@@ -18,7 +18,8 @@ const EventRecommender = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/recommend/predict-event", formData); 
+      const res = await axios.post("http://localhost:5000/api/recommend/predict-event", formData);
+
 
       setRecommendedEvent(res.data.recommended_event);
     } catch (error) {
